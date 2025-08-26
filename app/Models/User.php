24f71a09fc05d59eb8999a6d6c734f2b723e472d
victor <nilvictor99@gmail.com>
2025-08-26 18:20:19 -0500

    Fix formatting and improve Spanish language files; add country code CSS styles

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\System\SecuritySystemTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,7 +14,9 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
     use HasRoles;
+    use SecuritySystemTrait;
 
     /**
      * The attributes that are mass assignable.
